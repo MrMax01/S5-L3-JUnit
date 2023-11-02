8fe096aed2c28ce8dc97f23afa082c63d7397639
MrMax01 <massimo.mauro00@gmail.com>
2023-11-02 14:53:26 +0100

    first commit

---
+++ src/main/java/massimomauro/S5L3JUnit/S5L3JUnitApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S5L3JUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L3JUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L3JUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S5L3JUnit/S5L3JUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S5L3JUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L3JUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
